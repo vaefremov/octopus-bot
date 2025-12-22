008d72257658637c7a0cb1587c7b6c5d3d59cd4a
Vladimir Efremov <vefremov@gmail.com>
2025-12-22 21:05:01 +0200

    PAssword may be provided in a separate config.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,12 +2,22 @@
 #  Backup script
 # This script is a one-time script that performs backups
 
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+CONFIG="$SCRIPT_DIR/backup.conf"
+
 WHERE_BACKUP=/hdd5/BACKUPS
+DB_NAME1=octopus
+export PGPASSWORD="password_should_be_secret"
+export PGUSER=postgres
+
+if [ -n "${CONFIG:-}" ] && [ -f "$CONFIG" ]; then
+  # shellcheck source=/dev/null
+  source "$CONFIG"
+fi
 
 echo "Starting backup process..."
 echo "Backup will be created here: $WHERE_BACKUP"
 sleep 1
-DB_NAME1=octopus
 BACKUP_NAME="${DB_NAME1}_"$(date +%Y-%m-%d_%H-%M-%S).sql.gz
 BACKUP_TO=$WHERE_BACKUP/$BACKUP_NAME
 echo "Backing up PostgreSQL database to $BACKUP_TO"
